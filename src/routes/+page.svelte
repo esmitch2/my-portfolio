29ee693423384b83c423da31b120a6e2c0b702fc
esmitch <esmitch@mit.edu>
2025-03-12 15:46:52 -0400

    Fix image on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 
         <p>Hi! My name is Erin Mitchell, I'm a student at MIT in the Master's of Transportation program. I like to travel and take pictures, and I took the photo below in Iceland.</p>
 
-        <img src="/images/mountain.jpeg" alt="A photo of a sunset over water" style="width:400px"> 
+        <img src="./images/mountain.jpg" alt="A photo of a mountain" style="width:400px"> 
     </div>
 
     <h2>My GitHub Stats:</h2>
